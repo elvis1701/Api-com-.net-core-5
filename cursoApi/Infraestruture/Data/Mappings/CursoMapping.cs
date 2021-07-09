e8c8f44568b31308b3d3ac83bcf6980b7a4db651
elvis.alves54@gmail.com <elvis.alves54@gmail.com>
2021-07-09 10:59:05 -0300

    Entities to the Database

---
+++ cursoApi/Infraestruture/Data/Mappings/CursoMapping.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cursoApi.Infraestruture.Data.Mappings
+{
+    public class CursoMapping
+    {
+    }
+}
